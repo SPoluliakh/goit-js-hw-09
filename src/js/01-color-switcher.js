b924691b634a7985d3770ceaef2f4ef6db6b14ff
SPoluliakh <S_Poluliakh@ukr.net>
2022-09-01 22:45:10 +0300

    1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const refs = {
+  startBtnEl: document.querySelector('[data-start]'),
+  stopBtnEl: document.querySelector('[data-stop]'),
+  bodyEl: document.body,
+};
+let intervalId = null;
 
+refs.startBtnEl.addEventListener('click', changeColor);
+refs.stopBtnEl.addEventListener('click', stopChangeColor);
+
+function changeColor() {
+  toActiveStopBtn();
+  toDisabledSartBtn();
+  intervalId = setInterval(() => {
+    let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return (refs.bodyEl.style.backgroundColor = color);
+  }, 1000);
+}
+
+function stopChangeColor() {
+  toDisabledStopBtn();
+  toActiveStartBtn();
+  clearInterval(intervalId);
+}
+
+function toActiveStopBtn() {
+  refs.stopBtnEl.removeAttribute('disabled');
+}
+function toDisabledStopBtn() {
+  refs.stopBtnEl.setAttribute('disabled', 'true');
+}
+function toActiveStartBtn() {
+  refs.startBtnEl.removeAttribute('disabled');
+}
+function toDisabledSartBtn() {
+  refs.startBtnEl.setAttribute('disabled', 'true');
+}
