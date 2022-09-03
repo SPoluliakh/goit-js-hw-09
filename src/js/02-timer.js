6140300d0d1ec9b3ba6a37526afc364d69a3dc7d
SPoluliakh <S_Poluliakh@ukr.net>
2022-09-03 21:17:30 +0300

    2 done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,12 +5,17 @@ import Notiflix from 'notiflix';
 const refs = {
   startBtnEl: document.querySelector('[data-start]'),
   inputEl: document.querySelector('input#datetime-picker'),
+  daysEl: document.querySelector('[data-days]'),
+  hoursEl: document.querySelector('[data-hours]'),
+  minutesEl: document.querySelector('[data-minutes]'),
+  secondsEl: document.querySelector('[data-seconds]'),
+  timerEl: document.querySelector('.timer'),
 };
 
 refs.startBtnEl.disabled = true;
-const currentDate = Date.now();
+let intervalId = null;
 
-console.log(currentDate);
+refs.startBtnEl.addEventListener('click', onStartBtn);
 
 const options = {
   enableTime: true,
@@ -18,7 +23,7 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] <= new Date()) {
+    if (selectedDates[0] <= Date.now()) {
       Notiflix.Notify.failure('Please choose a date in the future');
       refs.startBtnEl.disabled = true;
     } else {
@@ -27,8 +32,50 @@ const options = {
     console.log(selectedDates[0]);
   },
 };
-console.log(Number(options.defaultDate));
 
-const fp = flatpickr('input#datetime-picker', options);
+flatpickr(refs.inputEl, options);
 
-console.log(fp);
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function onStartBtn() {
+  refs.startBtnEl.disabled = true;
+  intervalId = setInterval(() => {
+    let countdown = new Date(refs.inputEl.value) - Date.now();
+
+    if (countdown >= 0) {
+      const { days, hours, minutes, seconds } = convertMs(countdown);
+      refs.daysEl.textContent = addLeadingZero(days);
+      refs.hoursEl.textContent = addLeadingZero(hours);
+      refs.minutesEl.textContent = addLeadingZero(minutes);
+      refs.secondsEl.textContent = addLeadingZero(seconds);
+      if (countdown <= 60000) {
+        refs.timerEl.style.color = 'tomato';
+      }
+    } else {
+      refs.timerEl.style.color = 'black';
+      Notiflix.Notify.success('Countdown finished');
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
